5706fae3d32aab4ba28627baef2ec98b18e21cff
Gavin Hurlbut <gjhurlbu@gmail.com>
2016-12-12 02:04:09 -0800

    Redo ADC monitoring code

--- libraries/Beirdo-common/src/adcread.cpp
+++ libraries/Beirdo-common/src/adcread.cpp
@@ -1,32 +1,7 @@
 #include <Arduino.h>
 #include "adcread.h"
 
-uint16_t adc_readings[8];	///< raw readings
 int16_t core_temperature;	///< in 1/10 degree C
-uint16_t vcc;			///< in millivolts
-
-uint16_t readVcc(void)
-{
-  // Read 1.1V reference against AVcc
-  // set the reference to Vcc and the measurement to the internal 1.1V reference
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA, ADSC)); // measuring
-
-  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
-  uint8_t high = ADCH; // unlocks both
-
-  uint32_t result = (high << 8) | low;
-
-  // result = (1100mV / Vcc) * 1023
-  // we want Vcc
-  // Vcc = 1100mV * 1023 / result
-  // Vcc = 1125300mV / result
-  result = 1125300L / result;
-  return (uint16_t)result; // Vcc in millivolts
-}
 
 int16_t readAvrTemperature(void)
 {
@@ -43,75 +18,153 @@ int16_t readAvrTemperature(void)
   int32_t result = (high << 8) | low;
 
   // 242mV (225 measured) = -45C, 380mV (353 measured) = 85C
-  // use linear regression to convert from measured value to celcius
-  result -= 225;
-  result *= 1200; // * 120, have 0.1 deg
-  result >>= 7;   // /= 128
-  result -= 450;
+  // use linear regression to convert from measured value to celcius * 10
+  result = map(result, 225, 353, -450, 850);
   return (int16_t)result;
 }
 
-uint32_t convertVoltage(uint8_t input, uint16_t factor)
-{
-  // reading is from ADC (0 - 1023)
-  // factor is 1000/gain - gain is Vcc / max voltage in the circuit
-  // return value is measured in millivolts
 
-  // reading = (voltage * gain) / Vcc * 1023
-  // reading = voltage * 1000 / factor / Vcc * 1023
-  // voltage = reading * factor * Vcc / 1000 / 1023
-  // voltage = ((reading * Vcc / 1023) * factor) / 1000
-  uint32_t value = (uint32_t)analogRead(input);
-  value *= vcc;
-  value /= 1023;
-  value *= factor;
-  value /= 1000;
-  return value;
+PowerMonitor::PowerMonitor(void)
+{
+    m_current = 0;
+    m_voltage = 0;
+    m_power = 0;
 }
 
-uint32_t convertCurrent(uint8_t input, uint16_t factor1, uint16_t factor2)
+bool PowerMonitor::readMonitor(void)
 {
-  // reading is from ADC (0 - 1023)
-  // factor1 is factor2 * 1000 / gain - gain is Vcc / max current in the circuit
-  // return value is measured in microamps
+    uint32_t current;
+    uint32_t voltage;
+    uint32_t power;
+    bool result;
 
-  // reading = (current * gain) / Vcc * 1023
-  // reading = current * 1000 * factor2 / factor1 / Vcc * 1023
-  // current = reading * factor1 * Vcc / factor2 / 1000 / 1023
-  // current = (((reading * Vcc / 1023) * factor1) / factor2) / 1000
-  // to get current in microamps.
-  // current = ((reading * Vcc / 1023) * factor1) / factor2
-  uint32_t value = (uint32_t)analogRead(input);
-  value *= vcc;
-  value /= 1023;
-  value *= factor1;
-  value /= factor2;
-  return value;
+    result = readCurrent(current);
+    if (result) {
+        result = readVoltage(voltage);
+    }
+    if (result) {
+        result = readPower(power);
+    }
+    if (result) {
+        m_current = current;
+        m_voltage = voltage;
+        m_power = power;
+    }
+    return result;
 }
 
-uint32_t calculatePower(uint32_t voltage, uint32_t current)
+
+INA219PowerMonitor::INA219PowerMonitor(uint8_t address, uint8_t maxVoltage,
+                                       uint16_t maxVShunt, uint16_t rShunt,
+                                       float maxIExpected) :
+        PowerMonitor(), m_device(INA219(address))
+{
+    m_address = address;
+
+    m_device.begin(m_address);
+    m_device.setCalibration(maxVoltage, maxVShunt, rShunt, maxIExpected);
+}
+
+volatile bool INA219PowerMonitor::readCurrent(uint32_t &value)
+{
+    value = m_device.getCurrent_mA();
+    return true;
+}
+
+volatile bool INA219PowerMonitor::readVoltage(uint32_t &value)
+{
+    value = m_device.getBusVoltage_mV();
+    return true;
+}
+
+volatile bool INA219PowerMonitor::readPower(uint32_t &value)
+{
+    value = m_device.getPower_mA();
+    return true;
+}
+
+
+ADS1115PowerMonitor::ADS1115PowerMonitor(uint8_t address, uint8_t current,
+                                         uint8_t voltage, uint8_t currentPga,
+                                         uint8_t voltagePga, float voltageGain,
+                                         float currentGain) :
+        PowerMonitor(), m_device(ADS1115(address))
+{
+    m_address = address;
+    m_currentInput = current;
+    m_voltageInput = voltage;
+    m_currentPga = currentPga;
+    m_voltagePga = voltagePga;
+    m_currentGain = currentGain;
+    m_voltageGain = voltageGain;
+    m_rawCurrent = 0;
+    m_rawVoltage = 0;
+    m_lastCurrent = 0;
+    m_lastVoltage = 0;
+
+    m_device.initialize();
+}
+
+bool ADS1115PowerMonitor::readCurrent(uint32_t &value)
+{
+    m_device.setGain(m_currentPga);
+    m_device.setMultiplexer(m_currentInput);
+    m_rawCurrent = m_device.getConversion(true);
+    m_lastCurrent = convertCurrent();
+    value = m_lastCurrent;
+    return true;
+}
+
+bool ADS1115PowerMonitor::readVoltage(uint32_t &value)
+{
+    m_device.setGain(m_voltagePga);
+    m_device.setMultiplexer(m_voltageInput);
+    m_rawVoltage = m_device.getConversion(true);
+    m_lastVoltage = convertVoltage();
+    value = m_lastVoltage;
+    return true;
+}
+
+bool ADS1115PowerMonitor::readPower(uint32_t &value)
+{
+    value = convertPower();
+    return true;
+}
+
+
+uint32_t ADS1115PowerMonitor::convertVoltage(void)
+{
+  // reading is from 16-bit signed ADC
+  // gain value is the external gain
+  //  i.e. 120V -> 3.3V is a gain of 0.0275
+  uint32_t value = (uint32_t)(m_rawVoltage < 0 : -m_rawVoltage : m_rawVoltage);
+  value *= m_device.getFullScale(m_voltageInput);
+  value >>= 15;
+  return (uint32_t)(float(value) / m_voltageGain);
+}
+
+uint32_t ADS1115PowerMonitor::convertCurrent(void)
+{
+  // reading is from 16-bit signed ADC
+  // gain value is the external gain
+  //  i.e. 10A -> 100mV is a gain of 0.01
+  uint32_t value = (uint32_t)(m_rawCurrent < 0 : -m_rawCurrent : m_rawCurrent);
+  value *= m_device.getFullScale(m_currentInput);
+  value >>= 15;
+  return (uint32_t)(float(value) / m_currentGain);
+}
+
+uint32_t ADS1115PowerMonitor::calculatePower(void)
 {
   // voltage in millivolts
-  // current in microamps
+  // current in milliamps
   // result in milliwatts
 
-  current >>= 6;  // We need to lose some bits to fit the calculation into 32 bits along the way
-  uint32_t result = voltage * current;
-  result /= 15625;  // divide result (in nanowatts down to milliwatts - factor of 1e6 = (15625 << 6)
+  // divide result (in microwatts) down to milliwatts
+  // - factor of 1e3 = (250 << 2)
+  uint32_t result = (m_lastVoltage >> 1) * (m_lastCurrent >> 1);
+  result /= 250;
   return result;
-}
-
-void ADCPoll(void)
-{
-  vcc = readVcc();
-  core_temperature = readAvrTemperature();
-
-  // Setup to read ADC readings with VCC reference
-  analogReference(DEFAULT);
-
-  for (int i = 0; i < 8; i++) {
-    adc_readings[i] = (uint16_t)analogRead(i);
-  }
 }
 
 // vim:ts=4:sw=4:ai:et:si:sts=4
--- libraries/Beirdo-common/src/adcread.h
+++ libraries/Beirdo-common/src/adcread.h
@@ -1,16 +1,71 @@
 #ifndef ADCREAD_H__
 #define ADCREAD_H__
 
-extern uint16_t adc_readings[8];	///< raw readings
-extern int16_t core_temperature;	///< in 1/10 degree C
-extern uint16_t vcc;			///< in millivolts
+#include <INA219.h>
+#include <ADS1115.h>
 
-uint16_t readVcc(void);
+extern int16_t core_temperature;	///< in 1/10 degree C
+
 int16_t readAvrTemperature(void);
-uint32_t convertVoltage(uint8_t input, uint16_t factor);
-uint32_t convertCurrent(uint8_t input, uint16_t factor1, uint16_t factor2);
-uint32_t calculatePower(uint32_t voltage, uint32_t current);
-void ADCPoll(void);
+
+class PowerMonitor {
+    public:
+        PowerMonitor(void);
+        bool readMonitor(void);
+        uint32_t current(void) { return m_current; };
+        uint32_t voltage(void) { return m_voltage; };
+        uint32_t power(void)   { return m_power; };
+    protected:
+        volatile bool readCurrent(uint32_t &value) = 0;
+        volatile bool readVoltage(uint32_t &value) = 0;
+        volatile bool readPower(uint32_t &value) = 0;
+        uint32_t m_current;
+        uint32_t m_voltage;
+        uint32_t m_power;
+};
+
+class INA219PowerMonitor : public PowerMonitor {
+    public:
+        INA219PowerMonitor(uint8_t address, uint8_t maxVoltage,
+                           uint16_t maxVShunt, uint16_t rShunt,
+                           float maxIExpected);
+    protected:
+        INA219 m_device;
+        volatile bool readCurrent(uint32_t &value);
+        volatile bool readVoltage(uint32_t &value);
+        volatile bool readPower(uint32_t &value);
+        uint8_t m_address;
+};
+
+class ADS1115PowerMonitor : public PowerMonitor {
+    public:
+        ADS1115PowerMonitor(uint8_t address, uint8_t current, uint8_t voltage,
+                            uint8_t currentPga, uint8_t voltagePga,
+                            float voltageGain, float currentGain);
+    protected:
+        ADS1115 m_device;
+        volatile bool readCurrent(uint32_t &value);
+        volatile bool readVoltage(uint32_t &value);
+        volatile bool readPower(uint32_t &value);
+        uint8_t m_address;
+        uint8_t m_currentInput;
+        uint8_t m_voltageInput;
+        uint8_t m_currentPga;
+        uint8_t m_voltagePga;
+        float m_currentGain;
+        float m_voltageGain;
+
+        int16_t m_rawCurrent;
+        int16_t m_rawVoltage;
+
+        uint32_t m_lastCurrent;
+        uint32_t m_lastVoltage;
+
+        uint32_t convertCurrent(void);
+        uint32_t convertVoltage(void);
+        uint32_t calculatePower(void);
+};
 
 #endif
+
 // vim:ts=4:sw=4:ai:et:si:sts=4

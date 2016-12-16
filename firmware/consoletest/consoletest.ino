9377fa3cd8fd306873937845d3296c05705af724
Gavin Hurlbut <gjhurlbu@gmail.com>
2016-12-16 02:28:46 -0800

    Add some LCD stuff to the console test

--- firmware/consoletest/consoletest.ino
+++ firmware/consoletest/consoletest.ino
@@ -3,12 +3,21 @@
 #include "sleeptimer.h"
 #include "serialcli.h"
 
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9340.h>
+
 // in ms
 #define LOOP_CADENCE 100
+
+#define LCD_CS 10
+#define LCD_DC 5
+#define LCD_RST 7
 
 const uint8_t EEMEM rf_link_id = 0;
 
 SleepTimer sleepTimer(LOOP_CADENCE);
+Adafruit_ILI9340 tft = Adafruit_ILI9340(LCD_CS, LCD_DC, LCD_RST);
 
 void setup() 
 {
@@ -16,6 +25,13 @@ void setup()
     SMCR = 0x00;
     
     Serial.begin(115200);
+
+    tft.begin();
+    tft.fillScreen(ILI9340_BLACK);
+    tft.fillScreen(ILI9340_RED);
+    tft.fillScreen(ILI9340_GREEN);
+    tft.fillScreen(ILI9340_BLUE);
+    tft.fillScreen(ILI9340_BLACK);
 
     cli.initialize();
 }

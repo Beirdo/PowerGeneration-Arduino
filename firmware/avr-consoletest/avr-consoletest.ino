1e9239495afb9d020d65b5e5fb7b7e9fb2efcb84
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-01-07 19:56:32 -0800

    Rename consoletest to avr-consoletest

---
+++ firmware/avr-consoletest/avr-consoletest.ino
@@ -0,0 +1,25 @@
+#include "serialcli.h"
+
+#include <SPI.h>
+#include <LowPower.h>
+
+// in ms
+#define LOOP_CADENCE 120
+#define SWAP_TIME 2000
+#define SWAP_COUNT (SWAP_TIME / LOOP_CADENCE)
+
+SerialCLI cli(Serial);
+
+void setup() 
+{
+    cli.initialize();
+}
+
+void loop() 
+{
+    cli.handleInput();
+    LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_ON,
+                  SPI_OFF, USART0_ON, TWI_OFF);
+}
+
+// vim:ts=4:sw=4:ai:et:si:sts=4
